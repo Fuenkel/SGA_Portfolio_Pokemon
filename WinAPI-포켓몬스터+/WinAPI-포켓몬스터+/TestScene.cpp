e6eb87c48f931756c08f243cabf387e03cf94d40
kthss01 <kthss01@naver.com>
2018-03-18 23:07:52 +0900

    솔루션 정리 (테스트 및 프로토타입 솔루션과 실제 포트폴리오 솔루션 구분)

---
+++ WinAPI-포켓몬스터+/WinAPI-포켓몬스터+/TestScene.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+#include "TestScene.h"
+
+
+TestScene::TestScene()
+{
+}
+
+
+TestScene::~TestScene()
+{
+}
+
+HRESULT TestScene::Init()
+{
+	return S_OK;
+}
+
+void TestScene::Release()
+{
+}
+
+void TestScene::Update()
+{
+}
+
+void TestScene::Render()
+{
+}
